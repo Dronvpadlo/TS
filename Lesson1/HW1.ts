0e38a78b2cfafa1e03230c1ffbfb1ae63155701b
User <doronyuk714@mail.com>
2024-06-04 06:58:18 +0300

    first HW

---
+++ Lesson1/HW1.ts
@@ -0,0 +1,33 @@
+//task1
+let hello : string = 'hello';
+let owu : string ='owu';
+let com : string ='com';
+let ua : string ='ua';
+let e : number = 1;
+let f : number= 10;
+let g: number = -999;
+let h : number = 123;
+let i : number = 3.14;
+let j : number = 2.7;
+let k : number = 16;
+let l : boolean = true;
+let m : boolean = false;
+
+console.log(hello,owu,com,ua,e,f,g,h,i,j,k,l,m);
+
+//task2
+let firstName : string = 'Doronyuk';
+let middleName : string ='Andriy';
+let lastName : string = 'Lyubomyrovych';
+let space : string = ' ';
+
+let person : string = firstName + space + middleName + space + lastName;
+
+console.log(person);
+
+//task3
+let a : number = 100;
+let b : string ='100';
+let c : boolean  = true;
+
+console.log(typeof a, typeof b, typeof c);
